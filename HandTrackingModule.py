abb443325d4d464a08adecb34d81dcc4ee12f997
adrianCo <adrian.cobo.mh@gmail.com>
2022-07-30 23:17:36 +0200

    v1.0.1
    Signed-off-by: adrianCo <adrian.cobo.mh@gmail.com>

---
+++ HandTrackingModule.py
@@ -0,0 +1,98 @@
+import cv2
+import mediapipe as mp
+import time
+
+
+class handDetector():
+    def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(
+            self.mode, self.maxHands, 1, self.detectionCon, self.trackCon)
+
+        self.mpDraw = mp.solutions.drawing_utils  # draw the hands points on the images
+
+        self.tipIds = [4, 8, 12, 16, 20]  # finger landmarks id tips
+
+    def findHands(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    # draw the conections between the hand points
+                    self.mpDraw.draw_landmarks(
+                        img, handLms, self.mpHands.HAND_CONNECTIONS)
+
+        return img
+
+    def findPosition(self, img, handNo=0, draw=True):
+
+        self.lmList = []
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(myHand.landmark):
+                # print(id,lm) print landmark values
+                h, w, c = img.shape
+                # landmark value center pixel. landmark = hand points
+                cx, cy = int(lm.x*w), int(lm.y*h)
+                self.lmList.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 7, (255, 0, 0), cv2.FILLED)
+
+        return self.lmList
+
+    def fingersUp(self):  # just correctly detect the thumb depending of the hand orientation
+        fingers = []
+
+        # Thumb (this condition is for the right hand).
+        # You can check what hand it's and and adapt the code
+        if self.lmList[self.tipIds[0]][1] > self.lmList[self.tipIds[0]-1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+
+        # rest fingers
+        for id in range(1, 5):
+            if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id]-2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+        return fingers
+
+
+def main():
+    pTime = 0
+    cTime = 0
+    cap = cv2.VideoCapture(0)
+    detector = handDetector()
+
+    while True:
+        # Press Q on keyboard to  exit
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+
+        success, img = cap.read()
+        img = detector.findHands(img)
+        lmList = detector.findPosition(img)
+        if len(lmList) != 0:
+            print(lmList[4])
+
+        cTime = time.time()
+        fps = 1/(cTime-pTime)
+        pTime = cTime
+
+        # print the fps
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN,
+                    3, (255, 0, 255, 3))
+
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+
+
+if __name__ == "__main__":
+    main()
---
+++ VirtualPainter.py
@@ -0,0 +1,112 @@
+from sre_constants import SUCCESS
+import cv2
+import numpy as np
+import time
+import os
+import HandTrackingModule as htm
+
+folderPath = "Header"
+myList = os.listdir(folderPath)  # gave us the name of the files on that folder
+overlayList = []
+
+for imPath in myList:
+    image = cv2.imread(f'{folderPath}/{imPath}')
+    overlayList.append(image)
+
+header = overlayList[0]
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
+
+pTime = 0
+cTime = 0
+
+detector = htm.handDetector(detectionCon=0.85)
+drawColor = (255, 0, 255)
+brushThicknes = 15
+xp, yp = 0, 0
+# here is where we are going to draw
+imgCanvas = np.zeros((720, 1280, 3), np.uint8)
+
+while True:
+
+    # Press Q on keyboard to  exit
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+        break
+
+    # 1. Import image
+    success, img = cap.read()
+    img = cv2.flip(img, 1)
+
+    # 2. Find Hand Landmarks
+    img = detector.findHands(img)
+    lmList = detector.findPosition(img, draw=False)
+
+    if len(lmList) != 0:
+        # tip of index and middle fingers
+        x1, y1 = lmList[8][1:]
+        x2, y2 = lmList[12][1:]
+
+        # 3. Check which fingers are up
+        fingers = detector.fingersUp()
+
+        # -  1 finger up -> draw
+        if fingers[1] and fingers[2]:
+            xp, yp = 0, 0
+            if y1 < 125:  # we are in the header
+                if 250 < x1 < 450:
+                    header = overlayList[3]
+                    drawColor = (255, 0, 255)
+                elif 550 < x1 < 750:
+                    header = overlayList[1]
+                    drawColor = (255, 0, 0)
+                elif 800 < x1 < 950:
+                    header = overlayList[2]
+                    drawColor = (0, 255, 0)
+                elif 1050 < x1 < 1200:
+                    header = overlayList[0]
+                    drawColor = (0, 0, 0)
+            cv2.rectangle(img, (x1, y1+25), (x2, y2-25),
+                          drawColor, cv2.FILLED)
+        # -  2 fingers up -> selection mode
+        if fingers[1] and fingers[2] == False:
+            cv2.circle(img, (x1, y1), brushThicknes, drawColor, cv2.FILLED)
+            print("Drawing mode")
+            if xp == 0 and yp == 0:
+                xp, yp = x1, y1
+
+            if drawColor == (0, 0, 0):
+                brushThicknes = 50
+            else:
+                brushThicknes = 15
+
+            cv2.line(img, (xp, yp), (x1, y1), drawColor, brushThicknes)
+            cv2.line(imgCanvas, (xp, yp), (x1, y1), drawColor, brushThicknes)
+            xp, yp = x1, y1
+
+    # create a mask with the region colored
+    imgGray = cv2.cvtColor(imgCanvas, cv2.COLOR_BGR2GRAY)
+    _, imgInv = cv2.threshold(imgGray, 50, 255, cv2.THRESH_BINARY_INV)
+
+    # add the mask and the image
+    imgInv = cv2.cvtColor(imgInv, cv2.COLOR_GRAY2BGR)
+    img = cv2.bitwise_and(img, imgInv)
+    img = cv2.bitwise_or(img, imgCanvas)
+
+    # knowing the mesures of our headers we can overlay the header to our image, because an image is just a matrix
+    img[0:125, 0:1280] = header
+    # img = cv2.addWeighted(img, 0.5, imgCanvas, 0.5, 0)
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    # print the fps
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN,
+                3, (255, 0, 255, 3))
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(10)
+
+cap.release()
+cv2.destroyAllWindows()
